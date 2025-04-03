95117c09d6a0e3e2e509077414253239f7ae9680
SDartayet <sofiadartayet@gmail.com>
2025-04-03 16:30:30 -0300

    Ran cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{ops::{Index, IndexMut, Shl}, slice::SliceIndex};
+use std::{
+    ops::{Index, IndexMut, Shl},
+    slice::SliceIndex,
+};
 
 //The number of memory addresses is 2^16
 const MAX_MEMORY_ADDRESS: u16 = u16::MAX;
@@ -17,20 +20,24 @@ enum GeneralPurposeRegister {
     R4 = 4,
     R5 = 5,
     R6 = 6,
-    R7 = 7
+    R7 = 7,
 }
 
 struct LC3VM {
-    general_registers: [u16;8],
+    general_registers: [u16; 8],
     program_counter: u16,
-    condition_flags: [u16;3],
-    memory: [u16;MAX_MEMORY_ADDRESS as usize]
+    condition_flags: [u16; 3],
+    memory: [u16; MAX_MEMORY_ADDRESS as usize],
 }
 
 impl LC3VM {
-
     fn new() -> LC3VM {
-        LC3VM { general_registers: [0;8], program_counter: 0, condition_flags: [0;3], memory: [0;MAX_MEMORY_ADDRESS as usize] }
+        LC3VM {
+            general_registers: [0; 8],
+            program_counter: 0,
+            condition_flags: [0; 3],
+            memory: [0; MAX_MEMORY_ADDRESS as usize],
+        }
     }
 
     /// Updates the condition flags according to the result of the last arithmetic operations
@@ -39,7 +46,8 @@ impl LC3VM {
         let register_value = self.general_registers[register_number];
         if register_value == 0 {
             self.condition_flags[Flag::FlZero] = 1;
-        } else if register_value & 0b1000000000000000 != 0 { //In two's complement, if the first bit is one, the number is negative
+        } else if register_value & 0b1000000000000000 != 0 {
+            //In two's complement, if the first bit is one, the number is negative
             self.condition_flags[Flag::FlNeg] = 1;
         } else {
             self.condition_flags[Flag::FlPos] = 1;
@@ -50,16 +58,16 @@ impl LC3VM {
     /// Instruction structure: OPCODE (4 bits) | Destination register (3 bits) | Source register 1 (3 bits) | Mode (immediate or another register, 1 bit) | [00 | Source register 2 (3 bits)] on mode 0 or a 5 bit value on mode 1
     fn add(&mut self, instruction: lc3_instruction) {
         //I "push" the bits for the register number to the rightmost position, and make all the other bits 0 by doing a bitwise AND with 0b111
-        let source_register_1_number= ((instruction >> 6) & 0b111) as usize; 
+        let source_register_1_number = ((instruction >> 6) & 0b111) as usize;
 
         //Same as previously, but I need to shift right by 9 bits given there are 9 bits befrore the destination register
-        let destination_register_number = ((instruction >> 9) & 0b111) as usize; 
-        
+        let destination_register_number = ((instruction >> 9) & 0b111) as usize;
+
         //Mode flag is just one bit, so the bitwise AND is with 0b1. If it's zero I use register mode, if one immediate mode
         let mode_flag = (instruction >> 5) & 0b1 == 0;
 
         let second_operand: u16;
-        
+
         if mode_flag {
             // I get the number of the second register from the last 3 bits
             let source_register_2_number = (instruction & 0b111) as usize;
@@ -72,14 +80,19 @@ impl LC3VM {
             let sign_extension: u16;
 
             // If the sign extension is negative, I fill with 11 bits of 1, since 11+5 = 16. Otherwise I fill with zeroes
-            if is_immediate_negative { sign_extension = 0xFFE0; } else { sign_extension = 0x0000; }
+            if is_immediate_negative {
+                sign_extension = 0xFFE0;
+            } else {
+                sign_extension = 0x0000;
+            }
 
             // Doing a bitwise or between the sign extension I need and the immediate value I get the immediate with the sign extended
             second_operand = sign_extension | five_bit_immediate;
         }
 
         //Wrapping add lets us recreate the way addition works in two's complement systems while keeping the values unsigned (for more generalization)
-        self.general_registers[destination_register_number] = self.general_registers[source_register_1_number].wrapping_add(second_operand);
+        self.general_registers[destination_register_number] =
+            self.general_registers[source_register_1_number].wrapping_add(second_operand);
 
         self.update_flags(destination_register_number);
     }
@@ -88,12 +101,13 @@ impl LC3VM {
     /// Instruction structure: OPCODE (4 bits) | Destination register (3 bits) | Source register (3 bits) | 111111
     fn not(&mut self, instruction: lc3_instruction) {
         //I "push" the bits for the register number to the rightmost position, and make all the other bits 0 by doing a bitwise AND with 0b111
-        let source_register_number= ((instruction >> 6) & 0b111) as usize; 
+        let source_register_number = ((instruction >> 6) & 0b111) as usize;
 
         //Same as previously, but I need to shift right by 9 bits given there are 9 bits befrore the destination register
-        let destination_register_number = ((instruction >> 9) & 0b111) as usize; 
-        
-        self.general_registers[destination_register_number] = !self.general_registers[source_register_number];
+        let destination_register_number = ((instruction >> 9) & 0b111) as usize;
+
+        self.general_registers[destination_register_number] =
+            !self.general_registers[source_register_number];
 
         self.update_flags(destination_register_number);
     }
@@ -101,47 +115,48 @@ impl LC3VM {
     /// Implements the bitwise AND instruction
     /// Instruction structure: OPCODE (4 bits) | Destination register (3 bits) | Source register 1 (3 bits) | Mode (immediate or another register, 1 bit) | [00 Source register 2 (3 bits)] on mode 0 or a 5 bit value on mode 1
     fn and(&mut self, instruction: lc3_instruction) {
-       //I "push" the bits for the register number to the rightmost position, and make all the other bits 0 by doing a bitwise AND with 0b111
-       let source_register_1_number= ((instruction >> 6) & 0b111) as usize; 
+        //I "push" the bits for the register number to the rightmost position, and make all the other bits 0 by doing a bitwise AND with 0b111
+        let source_register_1_number = ((instruction >> 6) & 0b111) as usize;
 
-       //Same as previously, but I need to shift right by 9 bits given there are 9 bits befrore the destination register
-       let destination_register_number = ((instruction >> 9) & 0b111) as usize; 
-       
-       //Mode flag is just one bit, so the bitwise AND is with 0b1. If it's zero I use register mode, if one immediate mode
-       let mode_flag = (instruction >> 5) & 0b1 == 0;
+        //Same as previously, but I need to shift right by 9 bits given there are 9 bits befrore the destination register
+        let destination_register_number = ((instruction >> 9) & 0b111) as usize;
 
-       let second_operand: u16;
-       
-       if mode_flag {
-           let source_register_2_number = (instruction & 0b111) as usize;
-           second_operand = self.general_registers[source_register_2_number];
-       } else {
-           second_operand = instruction & 0b11111;
-       }
-       self.general_registers[destination_register_number] = self.general_registers[source_register_1_number] & second_operand;
+        //Mode flag is just one bit, so the bitwise AND is with 0b1. If it's zero I use register mode, if one immediate mode
+        let mode_flag = (instruction >> 5) & 0b1 == 0;
 
-       self.update_flags(destination_register_number);
+        let second_operand: u16;
+
+        if mode_flag {
+            let source_register_2_number = (instruction & 0b111) as usize;
+            second_operand = self.general_registers[source_register_2_number];
+        } else {
+            second_operand = instruction & 0b11111;
+        }
+        self.general_registers[destination_register_number] =
+            self.general_registers[source_register_1_number] & second_operand;
+
+        self.update_flags(destination_register_number);
     }
 }
 
 /// The opcodes for the instructions the architecture supports
 enum OpCode {
-    OpBR,   /* branch */
-    OpADD = 0b0001 << 12,  /* add  */
-    OpLD,   /* load */
-    OpST,   /* store */
-    OpJSR,  /* jump register */
-    OpAND = 0101 << 12,  /* bitwise and */
-    OpLDR,  /* load register */
-    OpSTR,  /* store register */
-    OpRTI,  /* unused */
-    OpNOT,  /* bitwise not */
-    OpLDI,  /* load indirect */
-    OpSTI,  /* store indirect */
-    OpJMP,  /* jump */
-    OpRES,  /* reserved (unused) */
-    OpLEA,  /* load effective address */
-    OpTRAP, /* execute trap */
+    OpBR,                 /* branch */
+    OpADD = 0b0001 << 12, /* add  */
+    OpLD,                 /* load */
+    OpST,                 /* store */
+    OpJSR,                /* jump register */
+    OpAND = 0101 << 12,   /* bitwise and */
+    OpLDR,                /* load register */
+    OpSTR,                /* store register */
+    OpRTI,                /* unused */
+    OpNOT,                /* bitwise not */
+    OpLDI,                /* load indirect */
+    OpSTI,                /* store indirect */
+    OpJMP,                /* jump */
+    OpRES,                /* reserved (unused) */
+    OpLEA,                /* load effective address */
+    OpTRAP,               /* execute trap */
 }
 
 enum Flag {
@@ -172,7 +187,6 @@ impl<T> IndexMut<Flag> for [T] {
     }
 }
 
-
 //I implement indexing arrays with register enum values to make code more declarative
 impl<T> Index<GeneralPurposeRegister> for [T] {
     type Output = T;
@@ -193,14 +207,14 @@ impl<T> Index<GeneralPurposeRegister> for [T] {
 impl<T> IndexMut<GeneralPurposeRegister> for [T] {
     fn index_mut(&mut self, idx: GeneralPurposeRegister) -> &mut Self::Output {
         match idx {
-            GeneralPurposeRegister::R0 => & mut self[0],
-            GeneralPurposeRegister::R1 => & mut self[1],
-            GeneralPurposeRegister::R2 => & mut self[2],
-            GeneralPurposeRegister::R3 => & mut self[3],
-            GeneralPurposeRegister::R4 => & mut self[4],
-            GeneralPurposeRegister::R5 => & mut self[5],
-            GeneralPurposeRegister::R6 => & mut self[6],
-            GeneralPurposeRegister::R7 => & mut self[7],
+            GeneralPurposeRegister::R0 => &mut self[0],
+            GeneralPurposeRegister::R1 => &mut self[1],
+            GeneralPurposeRegister::R2 => &mut self[2],
+            GeneralPurposeRegister::R3 => &mut self[3],
+            GeneralPurposeRegister::R4 => &mut self[4],
+            GeneralPurposeRegister::R5 => &mut self[5],
+            GeneralPurposeRegister::R6 => &mut self[6],
+            GeneralPurposeRegister::R7 => &mut self[7],
         }
     }
 }
@@ -210,10 +224,10 @@ fn main() {
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
+    use super::Flag::*;
+    use super::GeneralPurposeRegister::*;
     use super::*;
-    use super::GeneralPurposeRegister::{*};
-    use super::Flag::{*};
 
     #[test]
     fn add_writes_value_in_register_correctly_for_2_register_add() {
@@ -222,7 +236,8 @@ mod tests{
         vm.general_registers[R0] = 5;
         vm.general_registers[R1] = 4;
 
-        let add_instruction = (OpCode::OpADD as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | R0 as u16;
+        let add_instruction =
+            (OpCode::OpADD as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | R0 as u16;
 
         vm.add(add_instruction);
 
@@ -235,7 +250,8 @@ mod tests{
 
         vm.general_registers[R1] = 5;
 
-        let add_instruction = (OpCode::OpADD as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | (1 << 5) | 4;
+        let add_instruction =
+            (OpCode::OpADD as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | (1 << 5) | 4;
 
         vm.add(add_instruction);
 
@@ -248,11 +264,12 @@ mod tests{
 
         vm.general_registers[R1] = 5;
 
-        let add_instruction = (OpCode::OpADD as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | (1 << 5) | 011011; // 0b11111011 is -5 in two's complement
+        let add_instruction =
+            (OpCode::OpADD as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | (1 << 5) | 011011; // 0b11111011 is -5 in two's complement
 
         vm.add(add_instruction);
 
-        assert_eq!(vm.general_registers[R2] as i16,0);
+        assert_eq!(vm.general_registers[R2] as i16, 0);
     }
 
     #[test]
@@ -262,11 +279,12 @@ mod tests{
         vm.general_registers[R0] = 5;
         vm.general_registers[R1] = 0b1111111111111011; //-6 in two's complement
 
-        let add_instruction = (OpCode::OpADD as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | R0 as u16;
+        let add_instruction =
+            (OpCode::OpADD as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | R0 as u16;
 
         vm.add(add_instruction);
 
-        assert_eq!(vm.general_registers[R2] as i16,0);
+        assert_eq!(vm.general_registers[R2] as i16, 0);
     }
 
     #[test]
@@ -275,11 +293,12 @@ mod tests{
 
         vm.general_registers[R1] = 5;
 
-        let add_instruction = (OpCode::OpADD as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | (1 << 5) | 0b11010; // 0b11010 is -6 in two's complement with 5 bits
+        let add_instruction =
+            (OpCode::OpADD as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | (1 << 5) | 0b11010; // 0b11010 is -6 in two's complement with 5 bits
 
         vm.add(add_instruction);
 
-        assert_eq!(vm.condition_flags[FlNeg],1);
+        assert_eq!(vm.condition_flags[FlNeg], 1);
     }
 
     #[test]
@@ -288,11 +307,12 @@ mod tests{
 
         vm.general_registers[R1] = 5;
 
-        let add_instruction = (OpCode::OpADD as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | (1 << 5) | 0b11011; // 0b11011 is -5 in two's complement for a 5 bit value
+        let add_instruction =
+            (OpCode::OpADD as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | (1 << 5) | 0b11011; // 0b11011 is -5 in two's complement for a 5 bit value
 
         vm.add(add_instruction);
 
-        assert_eq!(vm.condition_flags[FlZero],1);
+        assert_eq!(vm.condition_flags[FlZero], 1);
     }
 
     #[test]
@@ -301,13 +321,13 @@ mod tests{
 
         vm.general_registers[R1] = 5;
 
-        let add_instruction = (OpCode::OpADD as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | (1 << 5) | 0b1; 
+        let add_instruction =
+            (OpCode::OpADD as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | (1 << 5) | 0b1;
 
         vm.add(add_instruction);
 
-        assert_eq!(vm.condition_flags[FlPos],1);
+        assert_eq!(vm.condition_flags[FlPos], 1);
     }
-
 
     #[test]
     fn and_writes_value_in_register_correctly_for_register_and_immediate() {
@@ -315,7 +335,8 @@ mod tests{
 
         vm.general_registers[R1] = 0b101;
 
-        let and_instruction = (OpCode::OpAND as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | (1 << 5) | 0b01001;
+        let and_instruction =
+            (OpCode::OpAND as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | (1 << 5) | 0b01001;
 
         vm.and(and_instruction);
 
@@ -329,7 +350,8 @@ mod tests{
         vm.general_registers[R0] = 0b1001;
         vm.general_registers[R1] = 0b101;
 
-        let and_instruction = (OpCode::OpAND as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | R0 as u16;
+        let and_instruction =
+            (OpCode::OpAND as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | R0 as u16;
 
         vm.and(and_instruction);
 
@@ -342,11 +364,12 @@ mod tests{
 
         vm.general_registers[R1] = 0xF000;
 
-        let and_instruction = (OpCode::OpADD as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | (1 << 5) | 0b11010; // 0b11111010 is -6 in two's complement
+        let and_instruction =
+            (OpCode::OpADD as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | (1 << 5) | 0b11010; // 0b11111010 is -6 in two's complement
 
         vm.and(and_instruction);
 
-        assert_eq!(vm.condition_flags[FlNeg],1);
+        assert_eq!(vm.condition_flags[FlNeg], 1);
     }
 
     #[test]
@@ -355,11 +378,12 @@ mod tests{
 
         vm.general_registers[R1] = 0;
 
-        let and_instruction = (OpCode::OpADD as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | (1 << 5) | 0b00110; // 0b11011 is -5 in two's complement for a 5 bit value
+        let and_instruction =
+            (OpCode::OpADD as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | (1 << 5) | 0b00110; // 0b11011 is -5 in two's complement for a 5 bit value
 
         vm.and(and_instruction);
 
-        assert_eq!(vm.condition_flags[FlZero],1);
+        assert_eq!(vm.condition_flags[FlZero], 1);
     }
 
     #[test]
@@ -368,11 +392,12 @@ mod tests{
 
         vm.general_registers[R1] = 5;
 
-        let and_instruction = (OpCode::OpADD as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | (1 << 5) | 0b1; 
+        let and_instruction =
+            (OpCode::OpADD as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | (1 << 5) | 0b1;
 
         vm.and(and_instruction);
 
-        assert_eq!(vm.condition_flags[FlPos],1);
+        assert_eq!(vm.condition_flags[FlPos], 1);
     }
 
     #[test]
@@ -394,11 +419,11 @@ mod tests{
 
         vm.general_registers[R0] = 0;
 
-        let not_instruction = (OpCode::OpADD as u16) | ((R1 as u16) << 9) | ((R0 as u16) << 6); 
+        let not_instruction = (OpCode::OpADD as u16) | ((R1 as u16) << 9) | ((R0 as u16) << 6);
 
         vm.not(not_instruction);
 
-        assert_eq!(vm.condition_flags[FlNeg],1);
+        assert_eq!(vm.condition_flags[FlNeg], 1);
     }
 
     #[test]
@@ -407,11 +432,11 @@ mod tests{
 
         vm.general_registers[R0] = 0xFFFF;
 
-        let not_instruction = (OpCode::OpADD as u16) | ((R1 as u16) << 9) | ((R0 as u16) << 6); 
+        let not_instruction = (OpCode::OpADD as u16) | ((R1 as u16) << 9) | ((R0 as u16) << 6);
 
         vm.not(not_instruction);
 
-        assert_eq!(vm.condition_flags[FlZero],1);
+        assert_eq!(vm.condition_flags[FlZero], 1);
     }
 
     #[test]
@@ -420,10 +445,10 @@ mod tests{
 
         vm.general_registers[R0] = 0xF000;
 
-        let not_instruction = (OpCode::OpADD as u16) | ((R1 as u16) << 9) | ((R0 as u16) << 6); 
+        let not_instruction = (OpCode::OpADD as u16) | ((R1 as u16) << 9) | ((R0 as u16) << 6);
 
         vm.not(not_instruction);
 
-        assert_eq!(vm.condition_flags[FlPos],1);
+        assert_eq!(vm.condition_flags[FlPos], 1);
     }
 }
