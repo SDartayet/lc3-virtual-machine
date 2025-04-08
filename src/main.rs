ead39d3c416d82fa81ab711e191d311d106290cb
SDartayet <sofiadartayet@gmail.com>
2025-04-08 14:52:50 -0300

    Fixed overflow bugs, added more functions for main loop

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,16 @@
+use std::{
+    env, fs::File, io::{stdin, stdout, Read, Write}, ops::{BitAnd, BitOr, BitOrAssign, Index, IndexMut, Shl}, os::fd::AsRawFd, path::Path, process::exit, slice::SliceIndex, sync::Arc
+};
+use ctrlc::*;
 use ::termios::tcgetattr;
+use termios::{tcsetattr, Termios, ECHO, ICANON, TCSANOW};
+use OpCode::*;
 use Flag::*;
 use GeneralPurposeRegister::*;
-use OpCode::*;
 use TrapCode::*;
-use ctrlc::*;
-use std::{
-    env,
-    fs::File,
-    io::{Read, Write, stdin, stdout},
-    ops::{BitAnd, BitOr, BitOrAssign, Index, IndexMut, Shl},
-    os::fd::AsRawFd,
-    path::Path,
-    process::exit,
-    slice::SliceIndex,
-    sync::Arc,
-};
-use termios::{ECHO, ICANON, TCSANOW, Termios, tcsetattr};
 
 //The number of memory addresses is 2^16
-const MAX_MEMORY_ADDRESS: usize = u16::MAX as usize;
+const MAX_MEMORY_ADDRESS: usize = 65536;
 const MEMORY_REGISTER_KEYBOARD_STATUS_ADDRESS: usize = 0xFE00;
 const MEMORY_REGISTER_KEYBOARD_DATA_ADDRESS: usize = 0xFE02;
 
@@ -213,7 +205,7 @@ impl LC3VM {
         }
 
         if condition_flag_is_up {
-            self.program_counter += program_counter_offset;
+            self.program_counter.wrapping_add(program_counter_offset);
         }
     }
 
@@ -432,7 +424,7 @@ impl LC3VM {
                 ((byte_2 << 8) | byte_1 as u16) as u16;
             offset += 1;
         }
-        return true;
+        true
     }
 }
 
@@ -494,7 +486,7 @@ impl From<u16> for OpCode {
             0b1100 => OpJMP,
             0b1110 => OpLEA,
             0b1111 => OpTRAP,
-            _ => OpRES,
+            _ => OpRES
         }
     }
 }
@@ -557,22 +549,23 @@ impl Index<usize> for MemoryArray {
     type Output = u16;
 
     fn index(&self, index: usize) -> &Self::Output {
-        if index == MEMORY_REGISTER_KEYBOARD_DATA_ADDRESS {
-            if check_key() {
-                self.0[MEMORY_REGISTER_KEYBOARD_STATUS_ADDRESS] = (1 << 15);
-                let mut character = [0; 1];
-                stdin().read_exact(&mut character);
-                self.0[MEMORY_REGISTER_KEYBOARD_DATA_ADDRESS] = character[0] as u16;
-            } else {
-                self.0[MEMORY_REGISTER_KEYBOARD_STATUS_ADDRESS] = 0;
-            }
-        }
         &self.0[index]
     }
 }
 
 impl IndexMut<usize> for MemoryArray {
+
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        if index == MEMORY_REGISTER_KEYBOARD_DATA_ADDRESS {
+            let mut input_buffer = [1;1];
+            stdin().read_exact(&mut input_buffer);
+            if input_buffer[0] != 0 {
+                self.0[MEMORY_REGISTER_KEYBOARD_STATUS_ADDRESS] = (1 << 15);
+                let mut character = [0; 1];
+                stdin().read_exact(&mut character);
+                self.0[MEMORY_REGISTER_KEYBOARD_DATA_ADDRESS] = character[0] as u16;
+            } else { self.0[MEMORY_REGISTER_KEYBOARD_STATUS_ADDRESS] = 0; }
+        }
         &mut self.0[index]
     }
 }
@@ -587,23 +580,27 @@ fn main() {
 
     disable_input_buffering(&mut original_tio);
     let args: Vec<String> = env::args().collect();
-    if (args.len() < 2) {
+    if (args.len() < 2)
+    {
+        let mut path = String::new();
+        stdin().read_line(&mut path);
         /* show usage string */
         print!("lc3 [image-file1] ...\n");
         exit(2);
     }
 
-    for j in 1..args.len() {
+    for j in 1.. args.len() {
         let file_path = Path::new(&args[j]);
 
-        if (vm.read_image_file(file_path)) {
+        if !vm.read_image_file(file_path)
+        {
             print!("failed to load image: {}\n", args[j]);
             exit(1);
         }
     }
     while vm.running {
         let instruction = OpCode::from(vm.memory[vm.program_counter as usize] >> 12);
-        vm.program_counter += 1;
+        vm.program_counter.wrapping_add(1);
         match instruction {
             OpADD => vm.add(),
             OpAND => vm.and(),
@@ -704,7 +701,7 @@ mod tests {
             (OpCode::OpADD as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | (1 << 5) | 0b11010; // 0b11010 is -6 in two's complement with 5 bits
         vm.memory[vm.program_counter as usize] = add_instruction;
         vm.memory[vm.program_counter as usize] = add_instruction;
-
+        
         vm.add();
 
         assert!(vm.condition_flags & FlNeg != 0);
@@ -718,7 +715,7 @@ mod tests {
 
         let add_instruction =
             (OpCode::OpADD as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | (1 << 5) | 0b11011; // 0b11011 is -5 in two's complement for a 5 bit value
-
+        
         vm.memory[vm.program_counter as usize] = add_instruction;
 
         vm.add();
@@ -752,7 +749,7 @@ mod tests {
             (OpCode::OpAND as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | (1 << 5) | 0b01001;
 
         vm.memory[vm.program_counter as usize] = and_instruction;
-
+        
         vm.and();
 
         assert_eq!(vm.general_registers[R2], 0b00001);
@@ -893,7 +890,7 @@ mod tests {
             (OpCode::OpADD as u16) | ((R2 as u16) << 9) | ((R1 as u16) << 6) | (1 << 5) | 0b11010; // 0b11010 is -6 in two's complement with 5 bits
 
         vm.memory[vm.program_counter as usize] = add_instruction;
-
+        
         vm.add();
 
         let branch_instruction = (OpCode::OpBR as u16) | (0b100 << 9) | 2;
@@ -902,7 +899,7 @@ mod tests {
 
         vm.branch();
 
-        assert_eq!(vm.program_counter, 2);
+        assert_eq!(vm.program_counter, 0x3002);
     }
 
     #[test]
@@ -924,7 +921,7 @@ mod tests {
 
         vm.branch();
 
-        assert_eq!(vm.program_counter, 2);
+        assert_eq!(vm.program_counter, 0x3002);
     }
 
     #[test]
@@ -946,7 +943,7 @@ mod tests {
 
         vm.branch();
 
-        assert_eq!(vm.program_counter, 2);
+        assert_eq!(vm.program_counter, 0x3002);
     }
 
     #[test]
@@ -968,7 +965,7 @@ mod tests {
 
         vm.branch();
 
-        assert_eq!(vm.program_counter, 2);
+        assert_eq!(vm.program_counter, 0x3002);
     }
 
     #[test]
@@ -990,7 +987,7 @@ mod tests {
 
         vm.branch();
 
-        assert_eq!(vm.program_counter, 2);
+        assert_eq!(vm.program_counter, 0x3002);
     }
 
     #[test]
@@ -1003,7 +1000,7 @@ mod tests {
 
         vm.branch();
 
-        assert_eq!(vm.program_counter, 0);
+        assert_eq!(vm.program_counter, 0x3000);
     }
 
     #[test]
@@ -1065,7 +1062,7 @@ mod tests {
         let load_instruction = (OpCode::OpLD as u16) | ((R0 as u16) << 9) | 32;
 
         vm.memory[vm.program_counter as usize] = load_instruction;
-
+        
         vm.load();
 
         assert_eq!(vm.program_counter, 10);
@@ -1084,7 +1081,7 @@ mod tests {
             (OpCode::OpLDR as u16) | ((R0 as u16) << 9) | ((R1 as u16) << 6) | 31;
 
         vm.memory[vm.program_counter as usize] = load_register_instruction;
-
+    
         vm.load_register();
 
         assert_eq!(vm.program_counter, 10);
@@ -1192,7 +1189,7 @@ mod tests {
         vm.program_counter = 5;
 
         let load_address_instruction = (OpCode::OpLEA as u16) | ((R0 as u16) << 9) | 0b111111010; // 0b111111010 is -6 in two's complement with 9 bits
-
+        
         vm.memory[vm.program_counter as usize] = load_address_instruction;
 
         vm.load_address();
@@ -1373,7 +1370,8 @@ mod tests {
     #[test]
     fn binary_file_is_read_correctly() {
         let mut vm = LC3VM::new();
-        vm.read_image_file(&Path::new("./binaries/test-data"));
+        vm.program_counter = 0;
+        vm.read_image_file(&Path::new("./binaries/test.obj"));
         let mut text_array = ['a'; 4];
         text_array[0] = (vm.memory[42] >> 8) as u8 as char;
         text_array[1] = (vm.memory[42] & 0xFF) as u8 as char;
