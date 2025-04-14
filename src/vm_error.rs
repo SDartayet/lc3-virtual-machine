e2fb7dd1192f4a983cc60ac78d43079730279d85
SDartayet <sofiadartayet@gmail.com>
2025-04-14 15:39:14 -0300

    Added documentation

--- src/vm_error.rs
+++ src/vm_error.rs
@@ -1,8 +1,14 @@
+/// Representation for the different errors that can arise in VM execution
 #[derive(Copy, Clone, Debug)]
 pub enum VMError {
+    /// Invalid opcode in the instruction encoding. Stores the instruction
     InvalidOpCode(u8),
+    /// Invalid trap code in the instruction encoding. Stores the instruction
     InvalidTrapCode(u8),
+    /// Error getting the terminal attributes in setup
     TerminalIOAttributesGet,
+    /// Error setting the terminal attributes in setup
     TerminalIOAttributesSet,
+    /// Error reading from stdin or writing to stdout
     IOError,
 }
--- src/vm_utils.rs
+++ src/vm_utils.rs
@@ -12,40 +12,69 @@ pub enum GeneralPurposeRegister {
     R6 = 6,
     R7 = 7,
 }
+
+///Code for a trap routine to be executed. The code is its location in memory
 pub enum TrapCode {
-    TrapGETC = 0x20,  /* get character from keyboard, not echoed onto the terminal */
-    TrapOUT = 0x21,   /* output a character */
-    TrapPUTS = 0x22,  /* output a word string */
-    TrapIN = 0x23,    /* get character from keyboard, echoed onto the terminal */
-    TrapPUTSP = 0x24, /* output a byte string */
-    TrapHALT = 0x25,  /* halt the program */
+    /// Get character from keyboard, not echoed onto the terminal 
+    TrapGETC = 0x20,  
+    /// Output a character 
+    TrapOUT = 0x21,   
+    /// Output a word string 
+    TrapPUTS = 0x22,  
+    /// Get character from keyboard, echoed onto the terminal 
+    TrapIN = 0x23,    
+    /// Output a byte string 
+    TrapPUTSP = 0x24, 
+    /// Halt the program 
+    TrapHALT = 0x25,  
 }
 
 /// The opcodes for the instructions the architecture supports
 #[derive(Debug, PartialEq, Eq)]
 pub enum OpCode {
-    OpBR = 0b0000 << 12,   /* branch */
-    OpADD = 0b0001 << 12,  /* add  */
-    OpLD = 0b0010 << 12,   /* load */
-    OpST = 0b0011 << 12,   /* store */
-    OpJSR = 0b0100 << 12,  /* jump register */
-    OpAND = 0b0101 << 12,  /* bitwise and */
-    OpLDR = 0b0110 << 12,  /* load register */
-    OpSTR = 0b0111 << 12,  /* store register */
-    OpNOT = 0b1001 << 12,  /* bitwise not */
-    OpLDI = 0b1010 << 12,  /* load indirect */
-    OpSTI = 0b1011 << 12,  /* store indirect */
-    OpJMP = 0b1100 << 12,  /* jump */
-    OpLEA = 0b1110 << 12,  /* load effective address */
-    OpTRAP = 0b1111 << 12, /* execute trap */
+    /// Branch 
+    OpBR = 0b0000 << 12,   
+     /// Add  
+    OpADD = 0b0001 << 12, 
+    /// Load 
+    OpLD = 0b0010 << 12,   
+    /// Store 
+    OpST = 0b0011 << 12,  
+    /// Jump register 
+    OpJSR = 0b0100 << 12,
+     /// Bitwise and   
+    OpAND = 0b0101 << 12, 
+    /// Load register 
+    OpLDR = 0b0110 << 12,  
+    /// Store register 
+    OpSTR = 0b0111 << 12,  
+    /// Bitwise not 
+    OpNOT = 0b1001 << 12,  
+    /// Load indirect 
+    OpLDI = 0b1010 << 12,  
+    /// Store indirect 
+    OpSTI = 0b1011 << 12,  
+    /// Jump 
+    OpJMP = 0b1100 << 12,  
+    /// Load effective address 
+    OpLEA = 0b1110 << 12, 
+    /// Execute trap 
+    OpTRAP = 0b1111 << 12, 
 }
 
+/// Values for the different condition flags for comparison with the cond register
 pub enum Flag {
-    Positive = 0b001, /* Set when the result of the previous operation was positive */
-    Zero = 0b010,     /* Set when the result of the previous operation was zero */
-    Negative = 0b100, /* Set when the result of the previous operation was negative */
+    /// Set when the result of the previous operation was positive
+    Positive = 0b001, 
+    /// Set when the result of the previous operation was zero
+    Zero = 0b010,     
+    /// Set when the result of the previous operation was negative
+    Negative = 0b100, 
 }
 
+/// Extends sign for an integer to make it 16 bits
+/// Input: an integer in two's complements and the number of bits representing it
+/// Output: the integer with its sign extended so it occupies 16 bits
 pub fn extend_sign_for_integer(value: u16, value_bit_count: u16) -> u16 {
     // If the first bit of the value is negative, because of how two's complement works, we need to extend it with ones unti lwe have 16 bits to preserve the sign
     // I check if the first bit of the value is one, and if it is I extend it with ones, otherwise with zeroes
