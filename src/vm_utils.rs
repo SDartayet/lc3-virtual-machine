df92faf68488f4641d69596469339a7681b17fce
SDartayet <sofiadartayet@gmail.com>
2025-04-14 15:40:04 -0300

    Ran cargo fmt

--- src/vm_utils.rs
+++ src/vm_utils.rs
@@ -15,61 +15,61 @@ pub enum GeneralPurposeRegister {
 
 ///Code for a trap routine to be executed. The code is its location in memory
 pub enum TrapCode {
-    /// Get character from keyboard, not echoed onto the terminal 
-    TrapGETC = 0x20,  
-    /// Output a character 
-    TrapOUT = 0x21,   
-    /// Output a word string 
-    TrapPUTS = 0x22,  
-    /// Get character from keyboard, echoed onto the terminal 
-    TrapIN = 0x23,    
-    /// Output a byte string 
-    TrapPUTSP = 0x24, 
-    /// Halt the program 
-    TrapHALT = 0x25,  
+    /// Get character from keyboard, not echoed onto the terminal
+    TrapGETC = 0x20,
+    /// Output a character
+    TrapOUT = 0x21,
+    /// Output a word string
+    TrapPUTS = 0x22,
+    /// Get character from keyboard, echoed onto the terminal
+    TrapIN = 0x23,
+    /// Output a byte string
+    TrapPUTSP = 0x24,
+    /// Halt the program
+    TrapHALT = 0x25,
 }
 
 /// The opcodes for the instructions the architecture supports
 #[derive(Debug, PartialEq, Eq)]
 pub enum OpCode {
-    /// Branch 
-    OpBR = 0b0000 << 12,   
-     /// Add  
-    OpADD = 0b0001 << 12, 
-    /// Load 
-    OpLD = 0b0010 << 12,   
-    /// Store 
-    OpST = 0b0011 << 12,  
-    /// Jump register 
+    /// Branch
+    OpBR = 0b0000 << 12,
+    /// Add  
+    OpADD = 0b0001 << 12,
+    /// Load
+    OpLD = 0b0010 << 12,
+    /// Store
+    OpST = 0b0011 << 12,
+    /// Jump register
     OpJSR = 0b0100 << 12,
-     /// Bitwise and   
-    OpAND = 0b0101 << 12, 
-    /// Load register 
-    OpLDR = 0b0110 << 12,  
-    /// Store register 
-    OpSTR = 0b0111 << 12,  
-    /// Bitwise not 
-    OpNOT = 0b1001 << 12,  
-    /// Load indirect 
-    OpLDI = 0b1010 << 12,  
-    /// Store indirect 
-    OpSTI = 0b1011 << 12,  
-    /// Jump 
-    OpJMP = 0b1100 << 12,  
-    /// Load effective address 
-    OpLEA = 0b1110 << 12, 
-    /// Execute trap 
-    OpTRAP = 0b1111 << 12, 
+    /// Bitwise and   
+    OpAND = 0b0101 << 12,
+    /// Load register
+    OpLDR = 0b0110 << 12,
+    /// Store register
+    OpSTR = 0b0111 << 12,
+    /// Bitwise not
+    OpNOT = 0b1001 << 12,
+    /// Load indirect
+    OpLDI = 0b1010 << 12,
+    /// Store indirect
+    OpSTI = 0b1011 << 12,
+    /// Jump
+    OpJMP = 0b1100 << 12,
+    /// Load effective address
+    OpLEA = 0b1110 << 12,
+    /// Execute trap
+    OpTRAP = 0b1111 << 12,
 }
 
 /// Values for the different condition flags for comparison with the cond register
 pub enum Flag {
     /// Set when the result of the previous operation was positive
-    Positive = 0b001, 
+    Positive = 0b001,
     /// Set when the result of the previous operation was zero
-    Zero = 0b010,     
+    Zero = 0b010,
     /// Set when the result of the previous operation was negative
-    Negative = 0b100, 
+    Negative = 0b100,
 }
 
 /// Extends sign for an integer to make it 16 bits
